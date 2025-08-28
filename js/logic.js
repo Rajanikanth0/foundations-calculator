79351aefe4e68b77c622b767f6cbf18fa97b5f17
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-28 13:47:19 +0530

    feat: Add num-pad keys

--- js/logic.js
+++ js/logic.js
@@ -12,3 +12,23 @@ const cal = {
     return this.cal_funs[op](+a, +b);
   }
 };
+
+// add keypad grid
+function getGrid() {
+  const cell_container = document.createDocumentFragment();
+  const num_keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.', '-'];
+
+  for (let index = 0; index < 12; index ++) {
+    const cell = document.createElement("div");
+    cell.textContent = num_keys[index];
+    cell.setAttribute("class", "num-key");
+
+    cell_container.appendChild(cell);
+  }
+
+  return cell_container;
+}
+
+const num_keys = getGrid();
+const num_keyBack = document.querySelector(".num-keys");
+num_keyBack.appendChild(num_keys);
